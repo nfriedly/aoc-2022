096787f4a8e2137b9c6f14ff192598f597dd828b
Nathan Friedly <nathan@nfriedly.com>
2022-12-06 10:23:08 -0500

    day 6, part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -2,23 +2,36 @@ use std::collections::{HashSet, VecDeque};
 
 fn main() {
     let input = include_str!("input.txt");
-    let marker = find_marker(input);
-    println!("first marker: {:?}", marker);
+    println!(
+        "start of packet marker: {:?}",
+        find_start_of_packet_marker(input)
+    );
+    println!(
+        "start of message marker: {:?}",
+        find_start_of_message_marker(input)
+    );
 }
 
-fn find_marker(input: &str) -> Option<usize> {
-    const WINDOW_SIZE: usize = 4;
-    let mut window: VecDeque<char> = VecDeque::with_capacity(WINDOW_SIZE);
-    println!("initial window len: {}", window.len());
+fn find_start_of_packet_marker(input: &str) -> Option<usize> {
+    find_marker(input, 4)
+}
+
+fn find_start_of_message_marker(input: &str) -> Option<usize> {
+    find_marker(input, 14)
+}
+
+// finds the first substring of [size] unique characters and returns the position of the final character in that substring
+fn find_marker(input: &str, size: usize) -> Option<usize> {
+    let mut window: VecDeque<char> = VecDeque::with_capacity(size);
     let char_indices = input.char_indices();
     for (index, char) in char_indices {
-        if window.len() >= WINDOW_SIZE {
+        if window.len() >= size {
             window.pop_front();
         }
         window.push_back(char);
-        if window.len() == WINDOW_SIZE {
+        if window.len() == size {
             let uniq = window.clone().into_iter().collect::<HashSet<_>>();
-            if uniq.len() == WINDOW_SIZE {
+            if uniq.len() == size {
                 return Some(index + 1); // +1 because counting starts at 1, but indexing starts at 0
             }
         }
@@ -30,24 +43,77 @@ fn find_marker(input: &str) -> Option<usize> {
 mod tests {
     use super::*;
 
+    // start of packet tests
     #[test]
     fn test_mjq() {
-        assert_eq!(find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(7));
+        assert_eq!(
+            find_start_of_packet_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
+            Some(7)
+        );
     }
     #[test]
     fn test_bvw() {
-        assert_eq!(find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+        assert_eq!(
+            find_start_of_packet_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"),
+            Some(5)
+        );
     }
     #[test]
     fn test_npp() {
-        assert_eq!(find_marker("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
+        assert_eq!(
+            find_start_of_packet_marker("nppdvjthqldpwncqszvftbrmjlhg"),
+            Some(6)
+        );
     }
     #[test]
     fn test_nzn() {
-        assert_eq!(find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
+        assert_eq!(
+            find_start_of_packet_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            Some(10)
+        );
     }
     #[test]
     fn test_zcf() {
-        assert_eq!(find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
+        assert_eq!(
+            find_start_of_packet_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            Some(11)
+        );
+    }
+
+    // start of message tests
+    #[test]
+    fn test_msg_mjq() {
+        assert_eq!(
+            find_start_of_message_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
+            Some(19)
+        );
+    }
+    #[test]
+    fn test_msg_bvw() {
+        assert_eq!(
+            find_start_of_message_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"),
+            Some(23)
+        );
+    }
+    #[test]
+    fn test_msg_npp() {
+        assert_eq!(
+            find_start_of_message_marker("nppdvjthqldpwncqszvftbrmjlhg"),
+            Some(23)
+        );
+    }
+    #[test]
+    fn test_msg_nzn() {
+        assert_eq!(
+            find_start_of_message_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            Some(29)
+        );
+    }
+    #[test]
+    fn test_msg_zcf() {
+        assert_eq!(
+            find_start_of_message_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            Some(26)
+        );
     }
 }
