5c2955836e4c81de67c419ca76e360ebd21179ff
Nathan Friedly <nathan@nfriedly.com>
2022-12-09 06:49:58 -0500

    got input parsing and dir sizing working

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,108 +1,67 @@
 use std::collections::HashMap;
 
-type Directory = HashMap<String, Entity>;
-
-#[derive(Debug)]
-enum Entity {
-    File(usize),
-    Dir(Directory),
-}
-fn get_directory<'a>(tree: &'a Directory, path: &Vec<&'a str>) -> &'a Directory {
-    tree
-}
-
-fn parse(input: &str) -> Option<Directory> {
-    let mut root: Directory = HashMap::new();
-    let mut dir = &mut root;
+fn get_dir_sizes_cumlative(input: &str) -> HashMap<String, usize> {
+    let mut dirs: HashMap<String, usize> = HashMap::new();
     let mut path: Vec<&str> = Vec::new();
     for line in input.lines() {
         println!("parsing '{}'", line);
-        use Entity::*;
-        let words: Vec<&str> = line.split_whitespace().collect();
         if line.starts_with("$ cd") {
             let dirname = line.split_at(5).1;
             if dirname == "/" {
-                path = Vec::new();
-                dir = &mut root;
+                path.clear();
+                path.push(dirname);
             } else if dirname == ".." {
                 path.pop();
-                dir = match dir.get_mut("..")? {
-                    Dir(dir) => dir,
-                    _ => &mut root,
-                }
             } else {
                 path.push(dirname);
-                dir = match dir.get_mut("..")? {
-                    Dir(dir) => dir,
-                    _ => panic!("dir {} (/{}) not found", dirname, path.join("/")),
-                }
             }
-            println!("path: /{}", path.join("/"));
-        } else if line.starts_with("$ ls") {
-            // do nothing
-            // dir = get_directory(&root, &path);
-        } else if line.starts_with("dir ") {
-            let name = line.split_at(4).1;
-            let new_dir: Directory = HashMap::from([("..".to_string(), Dir(dir))]);
-            dir.insert(name.to_string(), Dir(new_dir));
+            println!("path: {}", path.join("/"));
         } else {
-            if let Ok(size) = words.get(0)?.parse() {
-                dir.insert(words.get(1)?.to_string(), File(size));
-            } else {
-                println!("error parsing size");
-                return None;
+            if let Some((left, _)) = line.split_once(" ") {
+                if let Ok(file_size) = left.parse::<usize>() {
+                    //println!("size {}", size);
+                    for i in 0..path.len() {
+                        let dir = path[0..=i].join("/");
+                        println!("part {}: {:?}", i, dir);
+                        let dir_size = dirs.get(&dir).unwrap_or(&0);
+                        dirs.insert(dir, dir_size + file_size);
+                    }
+                }
+                // else it's a ls command or a directory in ls results
             }
         }
     }
-    Some(root)
+    dirs
+}
+
+fn sum_dirs_under_100k(dirs: HashMap<String, usize>) -> usize {
+    0
 }
 
 fn main() {
     let input = include_str!("input.txt");
 
-    let root = parse(input);
-    println!("parsed: {:?}", root);
+    let dir_sizes = get_dir_sizes_cumlative(input);
+    println!("parsed: {:?}", dir_sizes);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    //     let input = "$ cd /
-    // $ ls
-    // dir a
-    // 14848514 b.txt
-    // 8504156 c.dat
-    // dir d
-    // $ cd a
-    // $ ls
-    // dir e
-    // 29116 f
-    // 2557 g
-    // 62596 h.lst
-    // $ cd e
-    // $ ls
-    // 584 i
-    // $ cd ..
-    // $ cd ..
-    // $ cd d
-    // $ ls
-    // 4060174 j
-    // 8033020 d.log
-    // 5626152 d.ext
-    // 7214296 k"
-
-    // start of packet tests
     #[test]
-    fn test_path_start() {
+    fn test_parse() {
+        let input = include_str!("input-sample.txt");
+
+        let dir_sizes = get_dir_sizes_cumlative(input);
         assert_eq!(
-            Path::from("/")
-                .push("a")
-                .push("b")
-                .push("..")
-                .push("c")
-                .as_path(),
-            Path::from("/a/c")
+            dir_sizes,
+            HashMap::from([
+                ("/".to_string(), 48381165),
+                ("//a".to_string(), 94853),
+                ("//d".to_string(), 24933642),
+                ("//a/e".to_string(), 584)
+            ])
         );
     }
 }
