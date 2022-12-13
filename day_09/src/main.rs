06ffe792ff032f058c905827f45f8f1fa6d60349
Nathan Friedly <nathan@nfriedly.com>
2022-12-12 21:26:26 -0500

    day 9 WIP - input parsed

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,3 +1,39 @@
+#[derive(Debug)]
+enum Dir {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
+use Dir::*;
+
+#[derive(Debug)]
+struct Move {
+    dir: Dir,
+    dist: u8,
+}
+
+fn parse(input: &str) -> Vec<Move> {
+    input
+        .lines()
+        .map(|line| {
+            let (dir_str, dist_str) = line.split_once(" ").expect("no whitespace inline");
+            let dir = match dir_str {
+                "U" => Up,
+                "D" => Down,
+                "L" => Left,
+                "R" => Right,
+                _ => panic!("unexpected direction input"),
+            };
+            let dist = dist_str.parse().expect("unparseable distance");
+            Move { dir, dist }
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input-sample.txt");
+    let moves = parse(input);
+    println!("moves: {:#? }", moves);
 }
