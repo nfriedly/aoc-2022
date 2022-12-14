47a5abbb64d25e4bf5c70607b917872528869d50
Nathan Friedly <nathan@nfriedly.com>
2022-12-13 23:04:33 -0500

    day 9, part 2

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -26,7 +26,7 @@ fn parse(input: &str) -> Vec<Move> {
     input
         .lines()
         .map(|line| {
-            let (dir_str, dist_str) = line.split_once(" ").expect("no whitespace inline");
+            let (dir_str, dist_str) = line.trim().split_once(" ").expect("no whitespace inline");
             let dir = match dir_str {
                 "U" => Up,
                 "D" => Down,
@@ -70,16 +70,28 @@ fn step_t(t: Loc, h: Loc) -> Loc {
     Loc { x, y }
 }
 
-fn follow(moves: Vec<Move>) -> usize {
+fn follow(moves: Vec<Move>, num_knots: usize) -> usize {
     let mut h = Loc { x: 0, y: 0 };
-    let mut t = Loc { x: 0, y: 0 };
-    let mut tail_locations = HashSet::from([t]);
+    let mut knots: Vec<Loc> = Vec::with_capacity(num_knots);
+    for _ in 0..num_knots {
+        let t = Loc { x: 0, y: 0 };
+        knots.push(t);
+    }
+    let mut tail_locations = HashSet::from([knots[num_knots - 1]]);
     for m in moves {
+        //println!("move: {:?}", m);
         for _ in 0..(m.dist) {
             h = step_h(h, m.dir);
-            t = step_t(t, h);
-            tail_locations.insert(t);
+            let knot = step_t(knots[0], h);
+            knots[0] = knot;
+            for i in 1..num_knots {
+                let knot = step_t(knots[i], knots[i - 1]);
+                knots[i] = knot;
+            }
+            tail_locations.insert(knots[num_knots - 1]);
         }
+        //println!("results: {:?}, {:?}", h, knots);
+        //println!("tail locations: {:?}", tail_locations);
     }
     tail_locations.len()
 }
@@ -87,7 +99,7 @@ fn follow(moves: Vec<Move>) -> usize {
 fn main() {
     let input = include_str!("input.txt");
     let moves = parse(input);
-    let num_t_loc = follow(moves);
+    let num_t_loc = follow(moves, 9);
     println!("num tail locations: {}", num_t_loc);
 }
 
@@ -106,7 +118,30 @@ mod tests {
     fn test_step_follow() {
         let input = include_str!("input-sample.txt");
         let moves = parse(input);
-        let num_t_loc = follow(moves);
+        let num_t_loc = follow(moves, 1);
         assert_eq!(num_t_loc, 13);
+    }
+
+    #[test]
+    fn test_multi_knot() {
+        let input = include_str!("input-sample.txt");
+        let moves = parse(input);
+        let num_t_loc = follow(moves, 9);
+        assert_eq!(num_t_loc, 1);
+    }
+
+    #[test]
+    fn test_multi_knot_larger() {
+        let input = "R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20";
+        let moves = parse(input);
+        let num_t_loc = follow(moves, 9);
+        assert_eq!(num_t_loc, 36);
     }
 }
